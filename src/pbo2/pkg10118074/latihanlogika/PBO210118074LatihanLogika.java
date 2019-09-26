02fb2270fc57dd69c9794f1f39f8d70aaa07a623
fikrimaualana <55828126+Fikrimaulana15@users.noreply.github.com>
2019-09-26 17:52:23 +0700

    selesai

---
+++ src/pbo2/pkg10118074/latihanlogika/PBO210118074LatihanLogika.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118074.latihanlogika;
+
+/**
+ *
+ * @author 
+ * NAMA : FikriMaulana
+ * KELAS: IF-2
+ * NIM : 10118074
+ */
+public class PBO210118074LatihanLogika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b)" + ! (a && b));
+    }
+    
+}
